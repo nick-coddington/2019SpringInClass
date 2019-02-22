c8bf18f8bf980b81d79d89b1459ffa13e1d156fd
Nicholas Coddington <coddingn1@hawkmail.newpaltz.edu>
2019-02-21 20:21:35 -0500

    Working with SQL and and the server

---
+++ server/controllers/users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const conn = require('../models/mysql_connection')
+const app = express.Router();
+
+app.get("/", (req, res) => {
+
+    conn.query("SELECT * FROM 2019Spring_Persons", (err, data) => {
+        if(err) throw err;
+        res.send(data);
+    });
+    
+});
+
+
+module.exports = app;
---
+++ server/models/mysql_connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+              require('dotenv').load();
+
+const conn = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DB,
+    connectionLimit: 10
+});
+
+module.exports = conn;
