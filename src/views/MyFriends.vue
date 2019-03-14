14026c6a6d5e057d8740360f54b6f4850f73ebc9
Nicholas Coddington <coddingn1@hawkmail.newpaltz.edu>
2019-03-14 17:22:31 -0400

    Working with routes

---
+++ src/views/MyFriends.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <h1>My Friends Page</h1>
+    <ul>
+        <il>Friend 1</il>
+        <il>Friend 2</il>
+        <il>Friend 3</il>
+        <il>Friend 4</il>
+        <il>Friend 5</il>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
